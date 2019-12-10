bb3582fbc977f8e98b9d35a283a516617549da0e
MinexAutomation <minex.automation@gmail.com>
2019-12-10 11:53:44 -0800

    Added default machine location provider based on configuration.

---
+++ source/R5T.Hastings.Default/Code/Services/Implementations/MachineLocationProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.Extensions.Options;
+
+using R5T.Delos;
+
+using R5T.Hastings.Default.Configuration;
+
+
+namespace R5T.Hastings.Default
+{
+    public class MachineLocationProvider : IMachineLocationProvider
+    {
+        private IOptions<MachineConfiguration> MachineConfiguration { get; }
+
+
+        public MachineLocationProvider(IOptions<MachineConfiguration> machineConfiguration)
+        {
+            this.MachineConfiguration = machineConfiguration;
+        }
+
+        public MachineLocation GetMachineLocation()
+        {
+            var machineLocation = this.MachineConfiguration.Value.MachineLocation;
+            return machineLocation;
+        }
+    }
+}
