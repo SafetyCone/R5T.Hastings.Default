9c18c0f17728241699c36bbeff1bd10f027f2189
MinexAutomation <minex.automation@gmail.com>
2019-12-10 11:54:21 -0800

    Added UseX() method for the default machine location provider.

---
+++ source/R5T.Hastings.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Sardinia;
+
+using R5T.Hastings.Default.Configuration;
+
+using RawMachineConfiguration = R5T.Hastings.Default.Configuration.Raw.MachineConfiguration;
+
+
+
+namespace R5T.Hastings.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection UseDefaultMachineLocationProvider(this IServiceCollection services)
+        {
+            var configuration = services.GetConfiguration();
+
+            services
+                .Configure<RawMachineConfiguration>()
+                .ConfigureOptions<MachineConfiguration>()
+                .AddSingleton<IMachineLocationProvider, MachineLocationProvider>()
+                ;
+
+            return services;
+        }
+    }
+}
